827834c41884d7d8c7eeb2282661ff6ae936a9cd
Mike Hans <hansm@rpi.edu>
2014-06-27 12:30:54 -0400

    added the optimal_dca folder, containing a version of DCA that combines use of the cpu and gpu to reach a more efficient performance than the cpu or gpu could reach alone.

---
+++ optimal_DCA/d_code/deviceInitialize.h
@@ -0,0 +1,105 @@
+//	deviceInitialize.h
+//
+//This file contains the function used to find the initial zeta values of every body in the system
+
+//Included Files
+#include <stdio.h>
+#include<cuda.h>
+
+//Function Prototypes
+//	Functions found in deviceFuncts.h
+__device__ void MSsetup_gpu(float Minv[6][6],float S[6][6],int row, int col, float m, float I[],int Iindex);
+__device__ void Fa_gpu(float S[6][6],float w, float r[], float m,int row, int col);
+__device__ void zab_gpu(float r[],float z[6][6],float Minv[6][6],float S[6][6], int row, int col);
+__device__ void zaa_gpu(float r[],float z[6][6],float Minv[6][6],float S[6][6], int row, int col);
+__device__ void r02_gpu(float r[], float angle,float L);
+__device__ void r01_gpu(float r[], float angle,float L);
+__device__ void Mat61Mult_gpu(float A[6][6], float B[6][6], float C[6][6],int row, int col);
+__device__ void Mat66Mult_gpu(float A[6][6], float B[6][6], float C[6][6],int row, int col);
+
+//Initialize:
+//	Function used to find the initial zeta values for every body in the system.
+//		m is an array of the mass of each body in the system
+//		l is an array of the length of every body in the system
+//		I is an array holding the inertia tensors of all bodies in the system
+//		Zetas is the array that will hold all of the zeta values for all bodies
+//		n is the number of bodies
+__global__ void Initialize_gpu(float state[],float m[], float l[],float I[],float Zetas[],int n)
+{
+    //Variable Declarations
+	//	Variables to distinguish threads
+	const int i1 = blockIdx.x;
+	const int row = threadIdx.y;
+	const int col = threadIdx.x;
+
+	//	Indices to write solution
+    const int Iindex = blockIdx.x*3+row*3*n+col;
+    const int i11 = col+row*26*n+i1*26;
+    const int i12=i11+6;
+    const int i13=i12+6; //only col=0 can write
+    const int i21=i13+1;
+    const int i22=i21+6;
+    const int i23=i22+6; //only col = 0 can write
+
+	//	Temporary shared matrices for matrix operations
+    __shared__ float z[6][6];
+    __shared__ float Minv[6][6];
+    __shared__ float S[6][6];
+    __shared__ float r[3];
+    __shared__ float q;
+    __shared__ float w;
+
+    int i = 0;	//counter
+	q=0;
+	w=0;
+
+    while (i <= i1)	//Loop through the necessary amount of values
+    {
+        q+=state[i];	//Save the angle of the body
+        w+=state[i+n];	//Save the angular velocity of the body
+        i++;
+    }
+	__syncthreads();	//ensure w and q are found
+   
+    MSsetup_gpu(Minv,S,row,col,m[i1],I,Iindex);	//Set up the shifter and inverse mass matrix
+    z[row][col]=0;	//reset z
+    r01_gpu(r,q,l[i1]);	//Find the r01 position vector and save it in r
+    zaa_gpu(r,z,Minv,S,row,col);	//Find z11 and store it in z
+    Zetas[i11]=z[row][col];	//Save z11 into the output zeta array
+
+    z[row][col]=0;	//reset z
+    r02_gpu(r,q,l[i1]);	//Find the r02 position vector and save it in r
+	zab_gpu(r,z,Minv,S,row,col);	//Find z12 and store it in z
+    Zetas[i12]=z[row][col];	//Save z12 into the output zeta array
+
+    r01_gpu(r,q,l[i1]);	//Find the r01 position vector and save it in r
+    Fa_gpu(S,w,r,m[i1],row,col);	//Find the state dependent forces and save them in S
+    Mat61Mult_gpu(Minv,S,z,row,col);	//Perform Minv*S and save the result in z
+
+    if (col==0)	//Ensure only one column of threads enters the if
+    {
+        Zetas[i13]=z[row][col];	//Save z13 into the zeta matrix
+    }
+
+    z[row][col]=0;	//reset z
+    MSsetup_gpu(Minv,S,row,col,m[i1],I,Iindex);	//Set up the shifter and inverse mass matrix
+    r02_gpu(r,q,l[i1]);	//Find the r02 position vector
+    zaa_gpu(r,z,Minv,S,row,col);	//Find z22 and store it in z
+    Zetas[i22]=z[row][col];	//Save z22 in the zeta arary
+
+    z[row][col]=0;	//reset z
+    r01_gpu(r,q,l[i1]);	//Find the r01 position vector
+    zab_gpu(r,z,Minv,S,row,col);	//Find z21 and store it in z
+    Zetas[i21]=z[row][col];	//Save z21 in the zeta array
+ 
+    r02_gpu(r,q,l[i1]);	//Find the r02 position vector 
+    Fa_gpu(S,w,r,m[i1],row,col);	//Find the state dependent forces
+    Mat61Mult_gpu(Minv,S,z,row,col);	//Perorm Minv*S and save the result in z
+
+    if (col==0)	//Ensure only one column of threads enters the if
+    {
+        Zetas[i23]=z[row][col];	//save z23 in the zeta array
+    }
+
+	__syncthreads();	//Sync before finishing function
+}
---
+++ optimal_DCA/funct_bin/Disassemble.cu
@@ -0,0 +1,158 @@
+//	Disassemble.cu
+//
+//This file contains the function that disassembles bodies.
+
+//Included Files
+#include "classes.h"
+
+//Function Prototypes
+//	Functions found in DCAfuncts.cu
+void Mat61Mult(float A[6][6], float B[6][6], float C[6][6]);
+void make_W(float Xinv[6][6], float D[6][6]); 
+
+//Disassemble:
+//	Function used to disassemble bodies and solve for the forces and accelerations at the joints.
+//		lessbds is a list of the assembled bodies
+//		morebds is a list of the disassembled bodies
+//		oldAs is a list of the accelerations and forces for the assembled bodies
+//		newAs is a list of the accelerations and forces for the disassembled bodies
+//		num is the number of assembled bodies
+//		odd is equal to 1 if there is an odd number of disassembed bodies and equal to 
+//			0 if there is an even number.	
+void Disassemble(Body *lessbds,Body *morebds, float oldAs[] ,float newAs[], int num, int odd)
+{	
+	//Variable Declarations
+	//All variables are temporary variables used to perform matrix operations	
+	float Vals[6][6];
+	float temp1[6][6];
+	float temp2[6][6];
+	float A[6][6];
+	
+	for(int i=0, j=0; i<num-odd; i++, j+=2)	//Loop through every assembled body
+	{	
+
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+			for(int c=0; c< 6; c++)	//Loop through every column
+			{
+				temp1[r][c]=0;	//initialize temp1 to 0
+				temp2[r][c]=0;	//initialize temp2 to 0
+				
+				if(c<4)	//If the column is less than 4
+				{
+					Vals[r][c]=oldAs[c+r*(num)*4+i*4];	//Save the forces and accelerations for the
+				}										//current body in Vals
+    							
+				if(c==0) //If the column is 0
+				{
+					temp1[r][c]=morebds[j].z23[r]-morebds[j+1].z13[r]; //Save b1.z23+b2.z13 in temp1
+				}
+			}
+		}
+
+		
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+    		temp2[r][0]=Vals[r][3];	//Save b2.Fc2 into column 0 of temp2
+   		}
+
+    	Mat61Mult(morebds[j+1].z12,temp2,temp2);	//perform the operation b2.z12*temp2 and save
+		//temp 2 now holds b2.z12*b2.Fc2			//the result in temp2
+			
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+			temp2[r][0]=-1*temp2[r][0];	//make temp2 negative
+			//temp2 now holds -1*b2.z12*b2.Fc2
+
+			temp2[r][0]=temp2[r][0]+temp1[r][0];		//Add temp2 and temp1 and store the result 
+			//temp2 now holds -1*z212*F2c2+z123-z213	//in temp2
+
+			A[r][0]=Vals[r][1];	//Save b1.Fc1 in the first row of A
+    	}
+
+		Mat61Mult(morebds[j].z21,A,A);	//perform b1.z21*A and store the resul in A
+		//A now holds b1.z21*b1.Fc1
+		
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+			for(int c=0; c<6; c++)	//Loop through every column
+			{
+				if(c==0)	//If the column is 0
+				{
+    				temp2[r][c]=A[r][c]+temp2[r][c];	//add A to temp2
+					//temp2 now holds b1.21*b1.c1-b2.12*b2.c2+b1.23-b2.13
+				}
+    	
+    			if(r<5 && c<5)	//If the row and colum is less than 5
+    			{
+    				A[r][c]=lessbds[i].Xinv[r][c];	//Load X inverse into A
+    			}
+			}		
+		}
+		
+    	make_W(A,temp1);	//Use X inverse to create the intermediate matrix W
+    	//A now holds W
+
+    	Mat61Mult(A,temp2,A);	//Perform the operation A*temp2 and store the result in A
+		//A now holds W*(b1.21*b1.c1-b2.12*b2.c2+b1.23-b2.13)
+
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+    		A[r][0]=-1*A[r][0];	//A now holds -1*(W*((z121*F1c1)-(z212*F2c2)+z123-z213))
+			//A is now equal to b1.Fc2
+
+    		newAs[r*(num*2-odd)*4+i*8+3]=A[r][0];	//Save b1.Fc2 into the newAs list
+    		temp2[r][0]=Vals[r][1];	//Load b1.Fc1 into temp2
+    	}
+    	
+    	Mat61Mult(morebds[j].z21,temp2,temp1);	//Perform the operation b1.z21*temp2 and store the
+		//temp1 now holds b1.z21*b1.Fc1			//result in temp1
+    	
+		Mat61Mult(morebds[j].z22,A,temp2);	//Perform the operation b1.z22*A and store the 
+		//temp2 now holds b1.z22*b1.Fc2		//result in temp2
+
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+    		temp1[r][0]=temp1[r][0]+temp2[r][0];	//Add temp2 to temp1
+        	temp2[r][0]=morebds[j].z23[r];	//save b1.z23 into temp2
+    		temp2[r][0]=temp1[r][0]+temp2[r][0];	//temp2 now holds b1.A2
+        	newAs[r*(num*2-odd)*4+i*8+2]=temp2[r][0];	//Save b1.A2 in the newAs list
+    		A[r][0]=A[r][0]*-1;	//A now holds b2.Fc1
+        	newAs[r*(num*2-odd)*4+i*8+5]=A[r][0];	//Save b2.Fc1 in the newAs list
+			temp2[r][0]=Vals[r][3];	//Load b2.Fc2 into temp2
+   		}
+   
+    	Mat61Mult(morebds[j+1].z11,A,A);	//Perform the operation b2.z11*A and store the
+		//A now holds b2.z11*b2.Fc1			//result in A
+
+    	Mat61Mult(morebds[j+1].z12,temp2,temp2);	//Perform the operation b2.z12*temp2 and store
+		//temp2 now holds b2.z12*b2.Fc2				//the result in temp2
+		
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+			temp1[r][0]=A[r][0]+temp2[r][0]+morebds[j+1].z13[r];	//temp1 now holds b2.A1    	
+        	newAs[r*(num*2-odd)*4+i*8+4]=temp1[r][0];	//Save b2.A1 in the newAs list
+			
+			//Save all the values in oldAs into the correct spot in newAs
+        	newAs[r*(num*2-odd)*4+i*8]=Vals[r][0];
+        	newAs[r*(num*2-odd)*4+i*8+1]=Vals[r][1];
+        	newAs[r*(num*2-odd)*4+i*8+6]=Vals[r][2];
+        	newAs[r*(num*2-odd)*4+i*8+7]=Vals[r][3];
+    	}
+
+	}	//End loop through bodies
+
+	//If there is an odd number of bodies, the forces and accelerations from
+	//the last body in lessbds must be added to the newAs list
+	if(odd==1)
+	{
+		for(int r=0; r<6; r++)	//Loop through every row
+		{
+			//Save the forces and accelerations from the last body into the new list
+			newAs[(r+1)*(num*2-odd)*4-4]=oldAs[(r+1)*num*4-4];
+			newAs[(r+1)*(num*2-odd)*4-3]=oldAs[(r+1)*num*4-3];
+			newAs[(r+1)*(num*2-odd)*4-2]=oldAs[(r+1)*num*4-2];
+			newAs[(r+1)*(num*2-odd)*4-1]=oldAs[(r+1)*num*4-1];
+		}
+	}
+}
