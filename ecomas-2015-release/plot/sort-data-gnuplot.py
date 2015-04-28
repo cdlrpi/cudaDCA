de09c38a9719228d2c9c00739e9a0593257f310e
Jeremy Laflin <laflijj@gmail.com>
2015-04-28 12:00:51 -0400

    ECCOMAS release version working

---
+++ ecomas-2015-release/plot/sort-data-gnuplot.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+with open('../parallel-hybrid-speed-test/n.mtx') as file:
+    n = np.array([[float(digit) for digit in line.split()] for line in file],dtype=np.float64)
+with open('../parallel-hybrid-speed-test/t.mtx') as file:
+    tp = np.array([[float(digit) for digit in line.split()] for line in file],dtype=np.float64)
+with open('../serial-speed-test/t.mtx') as file:
+    ts = np.array([[float(digit) for digit in line.split()] for line in file],dtype=np.float64)
+
+
+# Output data in GNU-Plot friendly format
+#--------------------
+data = np.hstack((np.reshape(n[0],(n.shape[1],1)),ts.T,tp.T))
+np.savetxt('data.mat', data, fmt='%3.16f')
